04c063632085802c4342adaa53507e3ef9136da0
vatsal <vatsal@fineprint.legal>
2022-06-27 14:36:42 +0530

    update

--- cronjob.py
+++ cronjob.py
@@ -5,7 +5,7 @@ def check_paid_users_subscription_cron():
     headers = {
         "email-server-key": "et74a%^O2qnq55gDDfCFfbim%4m#Lca"
     }
-    url = 'http://127.0.0.1:8000/check_subscription_paid'
+    url = 'https://emailserver.ilaforplacements.com/check_subscription_paid'
     result = requests.post(url=url, headers=headers)
     print(result.json())
 
@@ -14,12 +14,11 @@ def check_free_users_subscription_cron():
     headers = {
         "email-server-key": "et74a%^O2qnq55gDDfCFfbim%4m#Lca"
     }
-    url = 'http://127.0.0.1:8000/check_subscription_free'
+    url = 'https://emailserver.ilaforplacements.com/check_subscription_free'
     result = requests.post(url=url, headers=headers)
     print(result.json())
 
 
 if __name__ == '__main__':
-    # check_free_users_subscription_cron()
-    # check_paid_users_subscription_cron()
-    pass  # delete this line before running code
+    check_free_users_subscription_cron()
+    check_paid_users_subscription_cron()

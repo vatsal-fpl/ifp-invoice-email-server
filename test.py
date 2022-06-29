a39fe8a922b6403d75d057fa3cf37f8b992f50f2
vatsal <vatsal@fineprint.legal>
2022-06-29 15:34:27 +0530

    update

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-from db import *
+def send_email_sendgrid():
+    import os
+    from sendgrid import SendGridAPIClient
+    from sendgrid.helpers.mail import Mail
 
-subscriptions = get_collection("ifp-b2c-prod", "subscription").find({})
-paid_users = [str(subscription.get('userId'))
-              for subscription in subscriptions]
-print(paid_users)
+    message = Mail(
+        from_email='vatsal@fineprint.legal',
+        to_emails='vatsal@fineprint.legal',
+        subject='ILA for Placements',
+        html_content='<strong>and easy to do anywhere, even with Python</strong>')
+    try:
+        sg = SendGridAPIClient(
+            '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')
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+
+
+send_email_sendgrid()
